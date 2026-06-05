5fd45289b8e4283984c2851c251f468dea9e75e7
nishasah6576-cmyk <nishasah6576@gmail.com>
2026-06-05 21:54:53 +0530

    edit code

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -1,5 +1,86 @@
 package testproject;
 
-public class test {
+import java.util.Scanner; // step 1
 
-}
+public class test {
+	
+
+		
+		public static void main(String[] args) {
+			
+			Scanner sc = new Scanner(System.in); // step 2
+			
+			System.out.print("enter your name : "); 
+			
+			char ch = sc.next().charAt(3); // function call
+			
+			System.out.println(" enter val is : " +ch); 
+			
+	 
+			//String name = sc.nextLine();
+			
+			//System.out.println(" your name is: " +name);
+			
+		}
+		
+	}
+
+			
+			
+			//int n , m;  (integer value)
+			
+			//System.out.print("enter a valu for n : ");
+			/*n = sc.nextInt();   
+
+			System.out.print("enter a valu for m : ");
+			m = sc.nextInt(); 
+			
+			
+	int sub ;
+			
+			sub = (n<m)?n:m;
+			
+			n = m*m;
+			
+			System.out.println(sub);
+			System.out.println(n);
+			
+
+		*/	
+		//}
+
+	//}
+		/*
+		 * package java.util;
+
+		{
+
+		String  --> next(); -->Can read 1 Word at a time
+		String  --> nextLine(); -->Can read Whole string
+		byte	--> nextByte();
+		short	--> nextShort();
+		int 	--> nextInt();
+		long 	--> nextLong();
+		float	--> nextFloat();
+		double	--> nextDouble();
+		boolean	--> nextBoolean();
+		char	--> next().charAt(0);
+
+
+		}
+
+
+		Syntax of Obj Creation:
+
+		ClassName objName = new ClassName();
+
+		Scanner inputBalaObj = new  Scanner(System.in);
+
+		*/
+		
+		
+		
+
+
+
+
